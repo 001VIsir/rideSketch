abdd67bc77bfd572e9750c1d7ce629bf35f72db2
VISIR <2561719535@qq.com>
2026-02-19 02:55:02 +0800

    Initial commit

---
+++ src/main/java/org/example/ridesketch/RideSketchApplication.java
@@ -0,0 +1,13 @@
+package org.example.ridesketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSketchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSketchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ridesketch/RideSketchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ridesketch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSketchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
